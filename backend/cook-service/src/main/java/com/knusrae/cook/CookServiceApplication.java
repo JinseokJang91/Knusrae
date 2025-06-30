dca4a7ebedbd692c70f2d6c83a2ae449a49d7eaa
Jinseok Jang <hya2089@gmail.com>
2025-06-30 23:46:08 +0900

    first commit

---
+++ backend/cook-service/src/main/java/com/knusrae/cook/CookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.knusrae.cook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookServiceApplication.class, args);
+	}
+
+}
